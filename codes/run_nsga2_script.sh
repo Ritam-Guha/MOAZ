37a11130c832eede27b924e5d8f24748e1b93dc1
Ritam Guha <ritamguha16gmail.com>
2023-04-18 12:44:00 -0400

    commit open source version

--- codes/run_nsga2_script.sh
+++
@@ -1,7 +0,0 @@
-for features in 4
-  do
-  for seed in $(seq 1 30);
-  do
-    sh run_demo_linreg_nsga2.sh $seed $features
-  done
-done
--- codes/run_re_script.sh
+++
@@ -1,7 +0,0 @@
-for features in 4
-  do
-  for seed in $(seq 1 30);
-  do
-    sh run_demo_linreg_re.sh $seed $features
-  done
-done
